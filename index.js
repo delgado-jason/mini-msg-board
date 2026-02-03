1d983af282cf580250c7648b813d43151ec11244
Jason Delgado <delgadorjason7@gmail.com>
2026-02-02 22:47:21 -0500

    Add New Messages

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 require('dotenv').config()
 
+const path = require('node:path')
 const express = require('express')
+const indexRouter = require('./routes/indexRouter')
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: true}))
+app.use('/', indexRouter)
+
+const PORT = process.env.PORT
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    console.log(`Listening on port ${PORT}`)
+})
